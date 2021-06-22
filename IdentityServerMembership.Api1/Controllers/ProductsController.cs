039973a79378ace33ca4cab753c2df4452eb6e7a
hasanfurkanfidan <furkanfidan.job@gmail.com>
2021-06-22 20:55:20 +0300

    ilerleme sağlandı

---
+++ IdentityServerMembership.Api1/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using IdentityServerMembership.Api1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerMembership.Api1.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var productList = new List<Product> { new Product { Id = 1, Name = "Product1" }, new Product { Id = 2, Name = "Product2" } };
+            return Ok(productList);
+        }
+    }
+}
---
+++ IdentityServerMembership.Api1/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerMembership.Api1.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
