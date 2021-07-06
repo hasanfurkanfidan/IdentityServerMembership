f9efd9790fb2504f0636d05c3f3e08a103501f3a
hasanfurkanfidan <furkanfidan.job@gmail.com>
2021-07-06 12:27:19 +0300

    custom user

---
+++ IdentityServerMembership.AuthServer/Models/CustomUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerMembership.AuthServer.Models
+{
+    public class CustomUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string City { get; set; }
+    }
+}
